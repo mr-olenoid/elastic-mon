a56875d4618edd7183708dd3cd30aa4b62bc61af
Чебоненко Сергій <serhii.chebonenko@ugv.com.ua>
2019-06-07 09:15:04 +0300

    logic update

--- main.py
+++ main.py
@@ -62,43 +62,70 @@ def msg_make_cpu(services, server_name, avg_cpu_used, origin, severity):
         table += "\n <i> %s used </i><b>%.2f%%</b> <i> of total cpu</i>" % (service["name"], service["cpu"] * 100)
     return json.dumps({'name': server_name, 'message': table, 'origin': origin, 'severity': severity, 'format': 'HTML'})
 
+server_falt = []
+server_high_memory = []
+server_high_cpu = []
+
 def logic(servers, config):
     es = Elasticsearch(config["elastic"]["server"], ssl_context=ssl_context, http_auth=(config["elastic"]["user"], config["elastic"]["password"]))
-    #rabbit
     credentials = pika.PlainCredentials(config["rabbit"]["user"], config["rabbit"]["password"])
     connection = pika.BlockingConnection(pika.ConnectionParameters(config["rabbit"]["server"], config["rabbit"]["port"], '/', credentials))#add to config
     channel = connection.channel()
     channel.exchange_declare(exchange='alarms', exchange_type='fanout')
 
     for srv in servers:
-        #print(srv)
         res = es.search(index="metricbeat*", body=elastic_helper.get_general(srv[0], "memory"))
         if res["hits"]["total"]["value"] < 4:
-            channel.basic_publish(exchange='alarms', 
-                                        routing_key='', 
-                                        body=json.dumps({'name': srv[0], 'message': "Too many lost data packets from: " + srv[0], 'origin': "windowsServers", 'severity': 'Error', 'format': 'HTML'}))
+            if srv not in server_falt:
+                server_falt.append(srv)
+                channel.basic_publish(exchange='alarms',
+                    routing_key='',
+                    body=json.dumps({'name': srv[0], 'message': "Too many lost data packets from: " + srv[0], 'origin': "windowsServers", 'severity': 'Error', 'format': 'HTML'}))
         else:
+            if srv in server_falt:
+                server_falt.remove(srv)
+                channel.basic_publish(exchange='alarms', 
+                                        routing_key='', 
+                                        body=json.dumps({'name': srv[0], 'message': "Server back in shape: " + srv[0], 'origin': "windowsServers", 'severity': 'Error', 'format': 'HTML'}))
+
+        if res["hits"]["total"]["value"] > 0:
             avg_memory_used = get_avg(res["hits"]["hits"], res["hits"]["total"]["value"], ["_source", "system", "memory", "actual", "used", "pct"])
             if avg_memory_used > 0.80:
-                res = es.search(index="metricbeat*", body=elastic_helper.get_general(srv[0], "process"))
-                services = group_services(res["hits"]["hits"], "memory")
-                channel.basic_publish(exchange='alarms', 
+                if srv not in server_high_memory:
+                    server_high_memory.append(srv)
+                    res = es.search(index="metricbeat*", body=elastic_helper.get_general(srv[0], "process"))
+                    services = group_services(res["hits"]["hits"], "memory")
+                    channel.basic_publish(exchange='alarms', 
+                                            routing_key='', 
+                                            body=msg_make(services, srv[0], avg_memory_used, "windowsServers", "warning"))
+            else:
+                if srv in server_high_memory:
+                    server_high_memory.remove(srv)
+                    channel.basic_publish(exchange='alarms', 
                                         routing_key='', 
-                                        body=msg_make(services, srv[0], avg_memory_used, "windowsServers", "warning"))
-                #print(srv)
+                                        body=json.dumps({'name': srv[0], 'message': "Server is no longer low on memory: " + srv[0], 'origin': "windowsServers", 'severity': 'Error', 'format': 'HTML'}))
 
             res = es.search(index="metricbeat*", body=elastic_helper.get_general(srv[0], "cpu"))
+            core_count = res["hits"]["hits"][0]["_source"]["system"]["cpu"]["cores"]
             avg_cpu_idle = get_avg(res["hits"]["hits"], 
                                     res["hits"]["total"]["value"], 
-                                    ["_source","system","cpu","idle","pct"])/res["hits"]["hits"][0]["_source"]["system"]["cpu"]["cores"]
+                                    ["_source","system","cpu","idle","pct"]) / core_count
             if avg_cpu_idle < 0.20:
-                # add message to rabbit
-                res = es.search(index="metricbeat*", body=elastic_helper.get_general(srv[0], "process"))
-                services = group_services(res["hits"]["hits"], "cpu")
-                channel.basic_publish(exchange='alarms', 
+                if srv not in server_high_cpu:
+                    server_high_cpu.append(srv)
+                    res = es.search(index="metricbeat*", body=elastic_helper.get_general(srv[0], "process"))
+                    services = group_services(res["hits"]["hits"], "cpu")
+                    for service in services:
+                        service["cpu"] = service["cpu"] / core_count
+                    channel.basic_publish(exchange='alarms', 
+                                            routing_key='', 
+                                            body=msg_make_cpu(services, srv[0], 1 - avg_cpu_idle, "windowsServers", "warning"))
+            else:
+                if srv in server_high_cpu:
+                    server_high_cpu.remove(srv)
+                    channel.basic_publish(exchange='alarms', 
                                         routing_key='', 
-                                        body=msg_make_cpu(services, srv[0], 1 - avg_cpu_idle, "windowsServers", "warning"))
-                print(srv)
+                                        body=json.dumps({'name': srv[0], 'message': "Server is no longer under load: " + srv[0], 'origin': "windowsServers", 'severity': 'Error', 'format': 'HTML'}))
 
 if __name__ == '__main__':
     ssl_context = create_ssl_context()
@@ -109,6 +136,7 @@ if __name__ == '__main__':
     with open('config.json', 'r') as f:
         config = json.load(f)
 
-    servers = db_helper.conf_loader_sql(config["database"]["server"], config["database"]["user"], config["database"]["password"], config["database"]["db_name"])
-    #print(servers)
-    logic(servers, config)
+    while True:
+        servers = db_helper.conf_loader_sql(config["database"]["server"], config["database"]["user"], config["database"]["password"], config["database"]["db_name"])
+        logic(servers, config)
+        time.sleep(60*5)
